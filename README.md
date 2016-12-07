cf636cf657ea1691d7760978b7a295709150426c
Ben Gyori <ben.gyori@gmail.com>
2016-12-07 17:39:21 -0500

    Files to prepare for release

---
+++ README.md
@@ -0,0 +1,20 @@
+PyKQML
+======
+
+PyKQML is an implementation of KQML messaging in Python. It consists of the following classes:
+
+    KQMLToken
+    KQMLString
+    KQMLList
+    KQMLPerformative
+    KQMLReader
+    KQMLDispatcher
+    KQMLModule
+
+PyKQML can be installed as
+
+    pip install pykqml
+
+You can import KQML classes as, for instance,
+
+    from kqml install KQMLList
---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(name='pykqml',
+    version='0.1',
+    packages=['kqml'],
+    keywords=['kqml', 'agent', 'nlp', 'communication', 'dialogue'],
+    url='http://github.com/bgyori/pykqml',
+    author='Benjamin M. Gyori',
+    author_email='benjamin_gyori@hms.harvard.edu',
+    description='KQML messaging classes in Python.',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        ]
+)
