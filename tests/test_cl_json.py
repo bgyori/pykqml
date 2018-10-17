b2bd1ccc30a77ee2fcc169be6b7d4cb6250d68dd
pagreene <patrick.anton.greene@gmail.com>
2018-10-17 17:23:28 -0400

    Test new reverse functionality.

--- tests/test_cl_json.py
+++ tests/test_cl_json.py
@@ -4,7 +4,10 @@ from kqml import cl_json, KQMLList
 def test_parse():
     json_dict = {'a': 1, 'b': 2,
                  'c': ['foo', {'bar': None, 'done': False}],
-                 'this is json': True}
-    res = cl_json.parse_json(json_dict)
+                 'this_is_json': True}
+    res = cl_json._cl_from_json(json_dict)
     assert isinstance(res, KQMLList)
     assert len(res) == 2*len(json_dict.keys())
+    back_dict = cl_json.cl_to_json(res)
+    assert len(back_dict) == len(json_dict)
+    # TODO: Should test for equality.
