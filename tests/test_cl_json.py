3bdd44347c33531b008df6f50646e679216b8c0f
pagreene <patrick.anton.greene@gmail.com>
2019-05-02 12:40:04 -0400

    Add more feedback on test failures.

--- tests/test_cl_json.py
+++ tests/test_cl_json.py
@@ -5,30 +5,36 @@ def _equal(json_val, back_json_val):
     if json_val is False and back_json_val is None:
         return True
     if type(json_val) != type(back_json_val):
+        print("Mismatched type:", type(json_val), type(back_json_val))
         return False
 
     if isinstance(json_val, dict):
         ret = True
         for key, value in json_val.items():
             if not _equal(value, back_json_val[key]):
+                print("Dict at %s:" % key, value, back_json_val[key])
                 ret = False
                 break
     elif isinstance(json_val, list):
         ret = True
         for i, value in enumerate(json_val):
             if not _equal(value, back_json_val[i]):
+                print("List at %s:" % i, value, back_json_val[i])
                 ret = False
                 break
     else:
         ret = (json_val == back_json_val)
+        if not ret:
+            print("Values not equal:", json_val, back_json_val)
     return ret
 
 
-def test_parse():
+def test_simple_parse():
     json_dict = {'a': 1, 'b': 2,
                  'c': ['foo', {'bar': None, 'done': False}],
                  'this_is_json': True}
     res = cl_json._cl_from_json(json_dict)
+    print(res.to_string())
     assert isinstance(res, KQMLList)
     assert len(res) == 2*len(json_dict.keys())
     back_dict = cl_json.cl_to_json(res)
